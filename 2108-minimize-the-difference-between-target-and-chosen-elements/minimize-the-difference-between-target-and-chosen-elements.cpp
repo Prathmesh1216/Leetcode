5f023ec7087946e4f1fa185022c6074350e1e233
Prathmesh1216 <110608902+Prathmesh1216@users.noreply.github.com>
2023-08-28 19:51:51 +0530

    Time: 1842 ms (40.25%) | Memory: 120 MB (15.60%) - LeetSync

---
+++ 2108-minimize-the-difference-between-target-and-chosen-elements/minimize-the-difference-between-target-and-chosen-elements.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int n,m;
+    int dp[71][70*70*70+1];
+    int solve(int i,int sum,int target,vector<vector<int>>& mat){
+        if(i>=n){
+            return abs(target-sum);
+        }
+        if(dp[i][sum]!=-1) return dp[i][sum];
+        int ans = INT_MAX;
+        for(int k = 0;k<m;k++){
+            ans = min(ans,solve(i+1,sum+mat[i][k],target,mat));
+        }
+        return dp[i][sum] = ans;
+    }
+    int minimizeTheDifference(vector<vector<int>>& mat, int target) {
+        n = mat.size();
+        m = mat[0].size();
+        memset(dp,-1,sizeof(dp));
+        int ans = INT_MAX;
+        return solve(0,0,target,mat);
+
+    }
+};
