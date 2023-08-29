6f4935905e890c821687d34e31ede204cc83dadc
Prathmesh1216 <110608902+Prathmesh1216@users.noreply.github.com>
2023-08-29 23:07:55 +0530

    Time: 265 ms (5.03%) | Memory: 73.2 MB (12.83%) - LeetSync

---
+++ 878-shifting-letters/shifting-letters.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string shiftingLetters(string s, vector<int>& shifts) {
+        int n = s.length();
+        vector<long long> ss(n);
+        ss[n-1] = shifts[n-1];
+        for(int i = n-2;i>=0;i--){
+            ss[i] = ss[i+1]+shifts[i];
+            cout << ss[i] << " ";
+        }
+        for(int i = 0;i<n;i++){
+            s[i] = char('a'+ (s[i]-'a'+ss[i])%26);
+        }
+        return s;
+    }
+};
