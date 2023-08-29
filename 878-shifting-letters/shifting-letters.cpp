67b728a8f7454353c4d9179d72a907961457196a
Prathmesh1216 <110608902+Prathmesh1216@users.noreply.github.com>
2023-08-29 23:08:30 +0530

    Time: 326 ms (5.03%) | Memory: 73.3 MB (8.86%) - LeetSync

--- 878-shifting-letters/shifting-letters.cpp
+++ 878-shifting-letters/shifting-letters.cpp
@@ -5,7 +5,7 @@ public:
         vector<long long> ss(n);
         ss[n-1] = shifts[n-1];
         for(int i = n-2;i>=0;i--){
-            ss[i] = ss[i+1]+shifts[i];
+            ss[i] = (ss[i+1]+shifts[i])%26;
             cout << ss[i] << " ";
         }
         for(int i = 0;i<n;i++){
