646e94945dd0766e49e12991a70e8a52b97f0c86
Prathmesh1216 <110608902+Prathmesh1216@users.noreply.github.com>
2023-09-01 20:05:35 +0530

    Time: 67 ms (69.08%) | Memory: 37.7 MB (43.85%) - LeetSync

---
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    static bool cmp(pair<int,int>& a,pair<int,int>& b){
+        if(a.first==b.first) return a.second<b.second;
+        return a.first>b.first;
+    }
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+        vector<pair<int,int>> v;
+        int n = profit.size();
+        for(int i = 0;i<n;i++){
+            v.push_back({profit[i],difficulty[i]});
+        }
+        sort(v.begin(),v.end(),cmp);
+        sort(worker.begin(),worker.end(),greater<int>());
+        int i = 0;
+        int j = 0;
+        int m = worker.size();
+        int ans  =0;
+        while(i<n && j<m){
+            if(worker[j]>=v[i].second){
+                ans += v[i].first;
+                j++;
+            }
+            else{
+                while(worker[j]<v[i].second){
+                    i++;
+                }
+            }
+        }
+        return ans;
+    }
+};
