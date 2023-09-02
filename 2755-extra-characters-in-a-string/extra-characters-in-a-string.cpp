602baa4c73a271b44c9b360a448358e2fa2ef724
Prathmesh1216 <110608902+Prathmesh1216@users.noreply.github.com>
2023-09-02 10:59:41 +0530

    Time: 270 ms (31.34%) | Memory: 92 MB (35.57%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     int dp[51];
     int solve(int i,string& s,set<string>& st){
-        if(i>=s.length()) return 0;
+        if(i==s.length()) return 0;
         if(dp[i]!=-1) return dp[i];
         int ans = 123456789;
-        string temp = "";
-        ans = 1 + solve(i+1,s,st);
         for(int j = i;j<s.length();j++){
-            temp += s[j];
-            if(st.find(temp)!=st.end()) ans = min(ans,solve(j+1,s,st));
+            string temp = s.substr(i,j-i+1);
+            int op1 = 123456789;
+            if(st.find(temp)==st.end()) op1 = j-i+1 + solve(j+1,s,st);
+            else op1 = solve(j+1,s,st);
+            ans = min(ans,op1);
         }
         return dp[i] = ans;
     }
