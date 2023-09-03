0f0d9ffde02e047019d204f74fe63bdb6a716bee
Prathmesh1216 <110608902+Prathmesh1216@users.noreply.github.com>
2023-09-03 11:29:52 +0530

    Time: 180 ms (5.06%) | Memory: 10.6 MB (42.75%) - LeetSync

--- 1184-car-pooling/car-pooling.cpp
+++ 1184-car-pooling/car-pooling.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
-    static bool cmp(vector<int>& a,vector<int>& b){
-        if(a[1]==b[1]) return a[2]<b[2];
-        return a[1]<b[1];
-    }
+static bool cmp(vector<int> &a,vector<int> &b){
+    if(a[1]==b[1]) return a[2]<b[2];
+    return a[1]<b[1];
+}
     bool carPooling(vector<vector<int>>& trips, int capacity) {
         sort(trips.begin(),trips.end(),cmp);
-        int n = trips.size();
-        int pc = 0;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        for(int i = 0;i<n;i++){
-            while(!pq.empty() && pq.top().first<=trips[i][1]){
-                pc-=pq.top().second;
-                pq.pop();
-            }
-            pq.push({trips[i][2],trips[i][0]});
-            pc+=trips[i][0];
-            if(pc>capacity) return false;
+        map<int,int> m;// des,passenger
+        int cap=0;
+      
+        
+        for(int i=0;i<trips.size();i++){
+            vector<int> temp2=trips[i];
+           
+    
+                for(auto it:m){
+                  cout<<"12";
+                    if(it.first<=temp2[1]) {
+                        cap-=it.second;
+                       
+                        m.erase(it.first);
+                    }
+                }
+               
+            
+        
+           
+                if(cap+temp2[0]>capacity) return false;
+                cap+=temp2[0];
+                cout<<endl;
+                cout<<"caps"<<cap<<endl;
+                
+            
+            m[temp2[2]]+=temp2[0];
+
         }
+    
         return true;
     }
 };
