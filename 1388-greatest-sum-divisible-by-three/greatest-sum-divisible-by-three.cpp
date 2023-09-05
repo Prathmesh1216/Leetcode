2e2cf38bfe717060be87ffbafe7d2b99d5a71a32
Prathmesh1216 <110608902+Prathmesh1216@users.noreply.github.com>
2023-09-05 19:34:52 +0530

    Time: 33 ms (87.67%) | Memory: 29.5 MB (65.85%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    map<pair<int,int>,int> mp;
+    int dp[40001][4];
     int solve(int i,int sum,vector<int>& nums){
         if(i==nums.size()){
             if(sum==0) return 0;
             return -123456789;
         }
-        if(mp.find({i,sum})!=mp.end()) return mp[{i,sum}];
-        return mp[{i,sum}] = max(solve(i+1,sum,nums),nums[i]+ solve(i+1,(sum+nums[i])%3,nums));
+        if(dp[i][sum]!=-1) return dp[i][sum];
+        return dp[i][sum] = max(solve(i+1,sum,nums),nums[i]+ solve(i+1,(sum+nums[i])%3,nums));
     }
     int maxSumDivThree(vector<int>& nums) {
+        memset(dp,-1,sizeof(dp));
         return solve(0,0,nums);
     }
 };
