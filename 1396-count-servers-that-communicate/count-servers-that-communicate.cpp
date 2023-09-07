dd3049d5713e592c98305dcf42be26901c2d751e
Prathmesh1216 <110608902+Prathmesh1216@users.noreply.github.com>
2023-09-07 22:12:53 +0530

    Time: 48 ms (70.08%) | Memory: 26 MB (11.87%) - LeetSync

--- 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
@@ -1,23 +1,69 @@
 class Solution {
 public:
+    vector<int> parent,size;
+    int findPar(int node){
+        if(parent[node]==node) return node;
+        return parent[node] = findPar(parent[node]);
+    }
+    void unionbs(int u,int v){
+        u = findPar(u);
+        v = findPar(v);
+        if(u==v){
+           return;
+        }
+        else if(size[v]<size[u]){
+            parent[v] = u;
+            size[u]+=size[v];
+            size[v] = 1;
+        }
+        else{
+            parent[u] = v;
+           size[v]+=size[u];
+           size[u] = 1;
+        }
+    }
     int countServers(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<int> row(n);
-        vector<int> col(m);
+        parent.resize(n*m);
+        size.resize(n*m,1);
+        for(int i = 0;i<n*m;i++){
+            parent[i] = i;
+        }
+     //   vector<int> row,col;
         for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
                 if(grid[i][j]==1){
-                    row[i]++;
-                    col[j]++;
+                    int node = i*m+j;
+                    while(j<m){
+                        if(grid[i][j]==1){
+                            unionbs(node,i*m+j);
+                        }
+                        j++;
+                    }
                 }
             }
         }
-        int ans = 0;
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(grid[i][j]==1) if(row[i]>1 || col[j]>1) ans++;
+        for(int j = 0;j<m;j++){
+            for(int i = 0;i<n;i++){
+                if(grid[i][j]){
+                    int node = i*m+j;
+                    while(i<n){
+                        if(grid[i][j]){
+                            unionbs(node,i*m+j);
+                        }
+                        i++;
+                    }
+                }
             }
+        
+        }
+     //   for(auto it : size) cout << it << endl;
+
+        int ans = 0;
+        for(int i = 0;i<size.size();i++){
+            if(size[i]>1) ans += size[i];
+
         }
         return ans;
     }
