aeaead3b21f19888d4bbea072190ab01848d8bdd
Prathmesh1216 <110608902+Prathmesh1216@users.noreply.github.com>
2023-09-10 12:57:12 +0530

    Time: 7 ms (100.00%) | Memory: 19.3 MB (57.14%) - LeetSync

---
+++ 3092-minimum-moves-to-spread-stones-over-grid/minimum-moves-to-spread-stones-over-grid.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int solve(int i,vector<vector<int>>& garib,vector<vector<int>>& v){
+        if(i==garib.size()) return 0;
+        int ans = 123456789;
+        for(int j = 0;j<v.size();j++){
+            if(v[j][2]>0){
+                v[j][2]--;
+                ans = min(ans,abs(garib[i][0]-v[j][0]) + abs(garib[i][1]-v[j][1])+solve(i+1,garib,v));
+                v[j][2]++;
+            }
+        }
+        return ans;
+    }
+    int minimumMoves(vector<vector<int>>& grid) {
+        vector<vector<int>> v;
+        vector<vector<int>> garib;
+        for(int i = 0;i<3;i++){
+            for(int j = 0;j<3;j++){
+                if(grid[i][j]>1) v.push_back({i,j,grid[i][j]-1});
+                if(grid[i][j]==0) garib.push_back({i,j});
+            }
+        }
+       // cout << garib.size() << " " << v.size() << endl;
+        return solve(0,garib,v);
+
+
+    }
+};
