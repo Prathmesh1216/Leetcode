d8146c695543971503e84ab1ed263ab9fdefe148
Prathmesh1216 <110608902+Prathmesh1216@users.noreply.github.com>
2023-09-25 15:59:06 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (28.65%) - LeetSync

--- 2078-maximum-compatibility-score-sum/maximum-compatibility-score-sum.cpp
+++ 2078-maximum-compatibility-score-sum/maximum-compatibility-score-sum.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
+    int dp[9][(1<<9)];
     int solve(int i,int mask,int m,vector<vector<int>>& comp){
         if(i==m) return 0;
+        if(dp[i][mask]!=-1) return dp[i][mask];
         int ans  = 0;
         for(int j = 0;j<m;j++){
             if(!(mask&(1<<j))) ans = max(ans,comp[i][j] + solve(i+1,mask|(1<<j),m,comp));
         }
-        return ans;
+        return dp[i][mask] = ans;
     }
     int maxCompatibilitySum(vector<vector<int>>& students, vector<vector<int>>& mentors) {
         int m = students.size();
         int n = students[0].size();
+        memset(dp,-1,sizeof(dp));
         vector<vector<int>> comp(m,vector<int>(m,0));
         for(int i = 0;i<m;i++){
             for(int j = 0;j<m;j++){
@@ -19,12 +22,6 @@ public:
                 }
             }
         }
-        // for(int i = 0;i<m;i++){
-        //     for(int j = 0;j<m;j++){
-        //         cout << comp[i][j] << " ";
-        //     }
-        //     cout << endl;
-        // }
         return solve(0,0,m,comp);
     }
 };
