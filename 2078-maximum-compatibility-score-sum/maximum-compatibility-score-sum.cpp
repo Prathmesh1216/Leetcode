3d40c812f587c0848c8203b625916b3a0133741e
Prathmesh1216 <110608902+Prathmesh1216@users.noreply.github.com>
2023-09-25 16:29:48 +0530

    Time: 92 ms (61.35%) | Memory: 10.2 MB (40.96%) - LeetSync

--- 2078-maximum-compatibility-score-sum/maximum-compatibility-score-sum.cpp
+++ 2078-maximum-compatibility-score-sum/maximum-compatibility-score-sum.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    int dp[9][(1<<9)];
-    int solve(int i,int mask,int m,vector<vector<int>>& comp){
-        if(i==m) return 0;
-        if(dp[i][mask]!=-1) return dp[i][mask];
-        int ans  = 0;
-        for(int j = 0;j<m;j++){
-            if(!(mask&(1<<j))) ans = max(ans,comp[i][j] + solve(i+1,mask|(1<<j),m,comp));
-        }
-        return dp[i][mask] = ans;
+int scor(vector<int>& student,vector<int>& mentor){
+    int sum=0;
+    for(int i=0;i<student.size();i++) {
+        if(mentor[i]==student[i]) sum++;
     }
+   
+    return sum;
+}
+int f(int idx,int mask,vector<vector<int>>& students,vector<vector<int>>& mentors){
+    if (idx>=mentors.size() ) return 0;
+   
+    int ans = 0;
+    for(int i=0;i<students.size();i++){
+        if((mask & (1<<i))==0){
+            int score=scor(mentors[idx],students[i]);
+            ans=max(ans,score+f(idx+1,mask|(1<<i),students,mentors));
+        }
+
+    }
+    return ans;
+
+}
     int maxCompatibilitySum(vector<vector<int>>& students, vector<vector<int>>& mentors) {
-        int m = students.size();
-        int n = students[0].size();
-        memset(dp,-1,sizeof(dp));
-        vector<vector<int>> comp(m,vector<int>(m,0));
-        for(int i = 0;i<m;i++){
-            for(int j = 0;j<m;j++){
-                for(int k = 0;k<n;k++){
-                    if(students[i][k]==mentors[j][k]) comp[i][j]++;
-                }
-            }
-        }
-        return solve(0,0,m,comp);
-    }
+        return f(0,0,students,mentors);
+    }
 };
