f94c92d24b8eb855337a0aaa296ee20de7068b55
Prathmesh1216 <110608902+Prathmesh1216@users.noreply.github.com>
2023-09-27 23:16:57 +0530

    Time: 73 ms (70.51%) | Memory: 32.9 MB (61.41%) - LeetSync

--- 2477-number-of-ways-to-reach-a-position-after-exactly-k-steps/number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
+++ 2477-number-of-ways-to-reach-a-position-after-exactly-k-steps/number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
    int mod = 1e9+7;
-   long long dp[4001][1001];
+   long long dp[3001][1001];
     long long solve(int i,int target,int k){
         if(target-i>k) return 0;
         if(k==0){
             if(i==target) return 1;
             return 0;
         }
-        if(dp[i+2000][k]!=-1) return dp[i+2000][k];
-        return dp[i+2000][k] = solve(i-1,target,k-1)%mod + solve(i+1,target,k-1)%mod;
+        if(dp[i+1000][k]!=-1) return dp[i+1000][k];
+        return dp[i+1000][k] = solve(i-1,target,k-1)%mod + solve(i+1,target,k-1)%mod;
     }
     int numberOfWays(int sp, int ep, int k) {
         if(ep<sp) swap(sp,ep);
