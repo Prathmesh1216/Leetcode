25f175817d07a602353842af0df83e395534f8b7
Prathmesh1216 <110608902+Prathmesh1216@users.noreply.github.com>
2023-09-28 13:38:40 +0530

    Time: 150 ms (85.71%) | Memory: 123.6 MB (33.53%) - LeetSync

--- 2085-array-with-elements-not-equal-to-average-of-neighbors/array-with-elements-not-equal-to-average-of-neighbors.cpp
+++ 2085-array-with-elements-not-equal-to-average-of-neighbors/array-with-elements-not-equal-to-average-of-neighbors.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        bool flag = true;
-        while(1){
-            flag = true;
-            for(int i = 1;i<nums.size()-1;i++){
-            double avg = 1.0*(nums[i-1]+nums[i+1])/2;
-          //  cout << avg << endl;
-            if(avg==nums[i]){
-                flag = false;
-                swap(nums[i],nums[i-1]);
-            } 
+        int n=nums.size();
+        for(int i=1;i<n-1;i++)
+        {
+            if((nums[i-1]<nums[i] && nums[i]<nums[i+1]) || (nums[i-1]>nums[i] && nums[i]>nums[i+1]))
+                swap(nums[i],nums[i+1]);
         }
-            if(flag) return nums;
-        }
-
         return nums;
     }
 };
