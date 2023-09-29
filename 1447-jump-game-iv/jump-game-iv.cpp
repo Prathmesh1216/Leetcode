ea602dc55968227cff30df7f79c7485a4deb645b
Prathmesh1216 <110608902+Prathmesh1216@users.noreply.github.com>
2023-09-30 01:02:54 +0530

    Time: 208 ms (37.81%) | Memory: 77.9 MB (49.77%) - LeetSync

---
+++ 1447-jump-game-iv/jump-game-iv.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minJumps(vector<int>& arr) {
+       int n = arr.size();
+       vector<int> adj[n];
+       map<int,vector<int>> mp;
+       for(int i = 0;i<n;i++){
+           mp[arr[i]].push_back(i);
+       }
+       vector<int> dist(n,123456789);
+       queue<pair<int,int>> q;
+       vector<bool> vis(n);
+       vis[0] = 1;
+       q.push({0,0});
+       while(!q.empty()){
+           int node = q.front().first;
+           int dist = q.front().second;
+           q.pop();
+           if(node==n-1) return dist;
+           if(node+1<n && !vis[node+1]) vis[node+1] = 1,q.push({node+1,dist+1});
+            if(node-1>=0 && !vis[node-1]) vis[node-1] = 1,q.push({node-1,dist+1});
+           for(auto& it : mp[arr[node]]){
+               if(!vis[it]) vis[it] = 1,q.push({it,dist+1});
+           }
+           mp[arr[node]].clear();
+       }
+       return -1;
+    }
+};
