11f091f2d78a53026c4a0ffdd4e82799c0427037
Prathmesh1216 <110608902+Prathmesh1216@users.noreply.github.com>
2023-09-30 11:23:19 +0530

    Time: 68 ms (58.88%) | Memory: 47 MB (88.05%) - LeetSync

--- 456-132-pattern/132-pattern.cpp
+++ 456-132-pattern/132-pattern.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     bool find132pattern(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> left(n);
-        left[0] = INT_MAX;
-        for(int i = 1;i<n;i++){
-            left[i] = min(left[i-1],nums[i-1]);
-        }
-        set<int> st;
-        st.insert(nums[n-1]);
-        for(int i = n-2;i>0;i--){
-            auto it = st.upper_bound(left[i]);
-            if(it==st.end() || *it>=nums[i]) st.insert(nums[i]);
-            else return true; 
-
+        stack<pair<int,int>> st;
+        int mini = nums[0]; 
+        for(int i=1;i<nums.size();i++)
+        {
+            while(!st.empty() && nums[i]>=st.top().first)
+            {
+                st.pop();
+            }
+            if(!st.empty() && nums[i]>st.top().second) return true;
+            st.push({nums[i], mini});
+            mini = min(mini, nums[i]);
         }
         return false;
     }
