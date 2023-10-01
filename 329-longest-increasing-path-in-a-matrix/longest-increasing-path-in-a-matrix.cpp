09e15ad6b90ae4d14d930ec60d3954b73295d69c
Prathmesh1216 <110608902+Prathmesh1216@users.noreply.github.com>
2023-10-01 19:37:11 +0530

    Time: 31 ms (86.51%) | Memory: 15.4 MB (90.81%) - LeetSync

---
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> moves = {{-1,0},{0,-1},{1,0},{0,1}};
+    int n,m;
+    bool isValid(int r,int c){
+        if(r<0 || r>=n || c<0 || c>=m) return false;
+        return true;
+    }
+    int dp[201][201];
+    int solve(int r,int c,vector<vector<int>>& matrix){
+        int ans = 1;
+        if(dp[r][c]!=-1) return dp[r][c];
+        for(auto& it : moves){
+            int nr = r + it[0],nc = c + it[1];
+            if(isValid(nr,nc) && matrix[nr][nc]>matrix[r][c]) ans = max(ans,1+solve(nr,nc,matrix));
+        }
+        return dp[r][c] = ans;
+    }
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        n = matrix.size(),m = matrix[0].size();
+        memset(dp,-1,sizeof(dp));
+        int ans = 0;
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                ans = max(ans,solve(i,j,matrix));
+            }
+        }
+        return ans;
+
+    }
+};
