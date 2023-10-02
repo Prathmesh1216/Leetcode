fb29358a51cf01fc717c97a5a2ae63973a1f4fc2
Prathmesh1216 <110608902+Prathmesh1216@users.noreply.github.com>
2023-10-02 16:33:37 +0530

    Time: 447 ms (84.78%) | Memory: 240 MB (27.60%) - LeetSync

--- 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
+++ 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
@@ -1,74 +1,76 @@
 class Solution {
 public:
-    int n,m;
-    vector<vector<int>> dist;
-    bool isValid(int i,int j){
-        if(i>=0 && i<n && j>=0 && j<m) return true;
-        return false;
-    }
-    bool Valid(int r,int c,int dr[],int dc[],int mid){
-        queue<pair<int,int>> q;
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        if(dist[r][c]>=mid) q.push({r,c});
-        while(!q.empty()){
-            int x = q.front().first;
-            int y = q.front().second;
-            q.pop();
-            if(x==n-1 && y==m-1) return true;
-            for(int i = 0;i<4;i++){
-                int nr = x + dr[i];
-                int nc = y + dc[i];
-                if(isValid(nr,nc) && !vis[nr][nc] && dist[nr][nc]>=mid){
-                    vis[nr][nc] = 1;
-                    q.push({nr,nc});
-                }
-            }
+ int row[4]={0,-1,0,1};
+int col[4]={-1,0,1,0};
+bool check(int r,int c,int maxi,vector<vector<int>> &dist,vector<vector<int>> &vis){
+     int n=dist.size();
+        int m=dist.size();
+        if(dist[r][c]<maxi) return false;
+    vis[r][c]=1;
+   
+    if(r==n-1 && c==m-1 ) return dist[r][c]>=maxi;
+    for(int i=0;i<4;i++){
+        int nr=r+row[i];
+        int nc=c+col[i];
+        if(nr>=0 && nr<n && nc>=0 && nc<m && !vis[nr][nc] && dist[nr][nc]>=maxi){
+            
+            if(check(nr,nc,maxi,dist,vis)) return true;
+
         }
-        return false;
+
     }
+    
+return false;
+
+}
+  
     int maximumSafenessFactor(vector<vector<int>>& grid) {
-        n = grid.size();
-        m = grid[0].size();
-        if(grid[0][0]==1 || grid[n-1][m-1]==1) return 0;
-        int drow[] = {-1,0,1,0};
-        int dcol[] = {0,-1,0,1};
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        dist.resize(n,vector<int>(m,INT_MAX));
-        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(grid[i][j]==1){
-                    dist[i][j] = 0;
-                    pq.push({0,i,j});
-                }
-            }
+      
+        int ans=0;
+        int n=grid.size();
+        int m=grid[0].size();
+          if(grid[0][0]==1) return 0;
+        if(grid[n-1][m-1]==1) return 0;
+       queue<pair<int,int>> q;
+       vector<vector<int>> dist(n,vector<int> (m,INT_MAX));
+       for(int i=0;i<n;i++){
+           for(int j=0;j<m;j++){
+               if(grid[i][j]==1) {
+                   dist[i][j]=0;
+                   q.push({i,j});
+               }
+           }
+       }
+       while(!q.empty()){
+           int r=q.front().first;
+           int c=q.front().second;
+           q.pop();
+           for(int i=0;i<4;i++){
+                int nr=r+row[i];
+               int nc=c+col[i];
+        if(nr>=0 && nr<n && nc>=0 && nc<m && dist[nr][nc]>1+dist[r][c]){
+            
+          dist[nr][nc]=1+dist[r][c];
+          q.push({nr,nc});
+
         }
-        while(!pq.empty()){
-            int dis = pq.top()[0];
-            int row = pq.top()[1];
-            int col = pq.top()[2];
-            pq.pop();
-            for(int i = 0;i<4;i++){
-                int nrow = row + drow[i];
-                int ncol = col + dcol[i];
-                if(isValid(nrow,ncol) && dist[nrow][ncol]>1+dist[row][col]){
-                    dist[nrow][ncol] = 1 + dist[row][col];
-                    pq.push({dist[nrow][ncol],nrow,ncol});
-                }
+
+           }
+       }
+   int l=0;
+   int h=n*m;
+       
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            vector<vector<int>> vis(n,vector<int> (m,0));
+            if(check(0,0,mid,dist,vis)){
+                ans=mid;
+                l=mid+1;
             }
+            else h=mid-1;
         }
-        int start = 0;
-        int end = max(n,m);
-        int ans = 0;
-        while(start<=end){
-            int mid = start + (end-start)/2;
-            if(Valid(0,0,drow,dcol,mid)){
-                ans  = mid;
-                start = mid + 1;
-            }
-            else end = mid - 1;
-        }
-        return ans;
+
+       return ans; 
         
     }
 };
