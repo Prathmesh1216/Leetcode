c245e4fd61fb658f37c38f95b084594fbe225b91
Prathmesh1216 <110608902+Prathmesh1216@users.noreply.github.com>
2023-10-03 14:53:05 +0530

    Time: 148 ms (57.46%) | Memory: 52.5 MB (68.32%) - LeetSync

--- 1308-smallest-string-with-swaps/smallest-string-with-swaps.cpp
+++ 1308-smallest-string-with-swaps/smallest-string-with-swaps.cpp
@@ -1,41 +1,56 @@
-class Solution {
+class DSU{
 public:
-    vector<int> parent,rank;
-    int findPar(int node){
-        if(parent[node] == node) return node;
-        return parent[node] = findPar(parent[node]);
+    vector<int> par, size;
+    DSU(int n)
+    {
+        par.resize(n+1);
+        iota(par.begin(), par.end(), 0);
+        size.resize(n+1, 1);
     }
-    void unionbr(int u,int v){
-        u = findPar(u);
-        v = findPar(v);
-        if(u==v) return;
-        if(rank[u]<rank[v]){
-            parent[u] = v;
-        }
-        else if(rank[v]<rank[u]){
-            parent[v] = u;
+    int findPar(int node)
+    {
+        if(par[node]==node) return node;
+        return par[node] = findPar(par[node]);
+    }
+    void Union(int x, int y)
+    {
+        int p1 = findPar(x), p2 = findPar(y);
+        if(p1==p2) return;
+        if(size[p1]<=size[p2])
+        {
+            par[p1] = p2;
+            size[p2] += size[p1];
         }
         else{
-            parent[v] = u;
-            rank[u]++;
+            par[p2] = p1;
+            size[p1] += size[p2];
         }
     }
-    string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
-        map<int,multiset<char>> mp;
-        int n = s.length();
-        parent.resize(n);rank.resize(n);
-        for(int i  = 1;i<n;i++) parent[i] = i;
-        for(auto& it : pairs){
-            unionbr(it[0],it[1]);
+};
+
+class Solution {
+public:
+    string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) 
+    {
+        int n = s.size();
+        DSU ds(n);
+        for(auto pr: pairs)
+        {
+            ds.Union(pr[0], pr[1]);
         }
-        for(int i = 0;i<n;i++){
-            mp[findPar(i)].insert(s[i]);
+        unordered_map<int, string> chars;
+        for(int i=0;i<n;i++)
+        {
+            chars[ds.findPar(i)].push_back(s[i]);
         }
-        for(int i = 0;i<n;i++){
-            auto it = mp[findPar(i)].begin();
-            s[i] = *it;
-            mp[findPar(i)].erase(it);
+        for(auto &[p, s]: chars) sort(s.begin(), s.end());
+        vector<int> pointers(n);
+        string ans;
+        for(int i=0;i<n;i++)
+        {
+            int p = ds.findPar(i);
+            ans.push_back(chars[p][pointers[p]++]);
         }
-        return s;
+        return ans;
     }
 };
