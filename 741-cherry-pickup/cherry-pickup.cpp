89e5ca2c69c4ce95e62263122d3dc63da2abc648
Prathmesh1216 <110608902+Prathmesh1216@users.noreply.github.com>
2023-10-05 01:08:49 +0530

    Time: 177 ms (23.01%) | Memory: 39.1 MB (22.30%) - LeetSync

--- 741-cherry-pickup/cherry-pickup.cpp
+++ 741-cherry-pickup/cherry-pickup.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
-    int n;
+    int n,m;
+    bool isValid(int i,int j,vector<vector<int>>& grid){
+        if(i>=n || j>=m || grid[i][j]==-1) return false;
+        return true;
+    }
     int dp[51][51][51][51];
-    int solve(int r1,int c1,int r2,int c2,vector<vector<int>>& grid)
-{
-    if(r1>=n || r2>=n || c1>=n || c2>=n || r1<0 ||r2 <0 || c1<0 || c2< 0 || grid[r1][c1]==-1 || grid[r2][c2]==-1) return -1e9;
-    
-    if(r1==n-1 && c1==n-1)  return grid[r1][c1];
-    if(r2==n-1 && c2==n-1) return grid[r2][c2];
-    if(dp[r1][c1][r2][c2]!=-1) return dp[r1][c1][r2][c2];
-    int ans=0;
-    if(r1==r2 && c1==c2) ans=grid[r1][c1];
-    else ans=grid[r1][c1]+grid[r2][c2];
-    ans +=  max(max(solve(r1+1,c1,r2+1,c2,grid),solve(r1,c1+1,r2,c2+1,grid)),max(solve(r1+1,c1,r2,c2+1,grid),solve(r1,c1+1,r2+1,c2,grid)));
-    return dp[r1][c1][r2][c2]= ans;
-}
-
+    vector<vector<int>> moves = {{1,0},{0,1}};
+    int solve(int i1,int j1,int i2,int j2,vector<vector<int>>& grid){
+        if(i1==n-1 && i2==n-1 && j1==m-1 && j2==m-1) return grid[i1][j1];
+        if(dp[i1][i2][j1][j2]!=-1) return dp[i1][i2][j1][j2];
+        int ans = 0;
+        if(i1==i2 && j1==j2) ans += grid[i1][j1];
+        else ans += grid[i1][j1] + grid[i2][j2];
+        int fans = -123456789;
+        if(isValid(i1+1,j1,grid) && isValid(i2+1,j2,grid)) fans = max(fans,ans + solve(i1+1,j1,i2+1,j2,grid));
+        if(isValid(i1,j1+1,grid) && isValid(i2+1,j2,grid)) fans = max(fans,ans + solve(i1,j1+1,i2+1,j2,grid));
+        if(isValid(i1+1,j1,grid) && isValid(i2,j2+1,grid)) fans = max(fans,ans + solve(i1+1,j1,i2,j2+1,grid));
+        if(isValid(i1,j1+1,grid) && isValid(i2,j2+1,grid)) fans = max(fans,ans + solve(i1,j1+1,i2,j2+1,grid));
+        return dp[i1][i2][j1][j2] = fans;
+    }
     int cherryPickup(vector<vector<int>>& grid) {
-        n = grid.size();
-        if(grid[0][0]==-1){
-            return 0;
-        }
-        if(grid[n-1][n-1]==-1){
-            return 0;
-        }
+        n = grid.size();m = grid[0].size();
         memset(dp,-1,sizeof(dp));
-        int ans = solve(0,0,0,0,grid);
-        if(ans<0) return 0;
-        return ans;
+        return (solve(0,0,0,0,grid)<=0)?0:solve(0,0,0,0,grid);
     }
 };
