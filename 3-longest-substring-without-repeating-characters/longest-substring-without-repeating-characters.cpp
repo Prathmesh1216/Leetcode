72b4a0846b135487b6637b9d6a39a40e61db41ab
Prathmesh1216 <110608902+Prathmesh1216@users.noreply.github.com>
2023-10-05 17:28:41 +0530

    Time: 12 ms (69.84%) | Memory: 9.4 MB (43.11%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -4,15 +4,13 @@ public:
         int i = 0;
         int j = 0;
         int ans = 0;
-        unordered_map<char,int> v;
+        unordered_map<char,int> mp;
         while(j<s.length()){
-            v[s[j]]++;
-            if(v[s[j]]>1){
-                while(v[s[j]]>1){
-                    v[s[i]]--;
-                    if(v[s[i]]==0) v.erase(s[i]);
-                    i++;
-                }
+            mp[s[j]]++;
+            while(mp.size()<j-i+1){
+                mp[s[i]]--;
+                if(mp[s[i]]==0) mp.erase(s[i]);
+                i++;
             }
             ans = max(ans,j-i+1);
             j++;
