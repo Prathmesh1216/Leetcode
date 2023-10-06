79430203f74323f4edff62bbfa9b457f64038c95
Prathmesh1216 <110608902+Prathmesh1216@users.noreply.github.com>
2023-10-06 19:17:00 +0530

    Time: 54 ms (17.62%) | Memory: 8.3 MB (34.11%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,54 +1,46 @@
 class Solution {
 public:
-    bool isValid(vector<int>& v1,vector<int>& v){
+    vector<int> vt;
+    bool isValid(vector<int>& v){
         for(int i = 0;i<52;i++){
-            if(v1[i]<v[i]) return false;
+            if(vt[i]>v[i]) return false; 
         }
         return true;
     }
     string minWindow(string s, string t) {
-        vector<int> v(52,0);
-        int ans = 123456789;
-        int tar = 0;
-        for(int i = 0;i<t.length();i++){
-            if(isupper(t[i])){
-                if(v[26 +(t[i]-'A')]==0) tar++;
-                v[26 +(t[i]-'A')]++;
+        vt.resize(52);
+        for(auto& it : t){
+            if(isupper(it)){
+                vt[26 + (it-'A')]++;
             }
-            else{
-                if(v[t[i]-'a']==0) tar++;
-                v[t[i]-'a']++;
-            } 
-        }
-        vector<int> v1(52,0);
+            else vt[it-'a']++;
+        } 
+        vector<int> v(52);
         int i = 0;
         int j = 0;
-        int st, e;
+        int ans = 123456789;
+        int ai = 0;
+        int aj = 0;
         while(j<s.length()){
             if(isupper(s[j])){
-                v1[26 +(s[j]-'A')]++;
+                v[26+(s[j]-'A')]++;
             }
-            else{
-                v1[s[j]-'a']++;
-            } 
-            while(isValid(v1,v) && i<=j){
+            else v[s[j]-'a']++;
+            while(isValid(v)){
                 if(j-i+1<ans){
-                    ans = j-i+1;
-                    st = i;
-                    e = j;
+                    ans  = j-i+1;
+                    ai = i;
+                    aj = j;
                 }
                 if(isupper(s[i])){
-                    if(v1[26 +(s[i]-'A')]>0) v1[26 +(s[i]-'A')]--;
+                    v[26+(s[i]-'A')]--;
                 }
-                else{
-                    if(v1[s[i]-'a']>0) v1[s[i]-'a']--;
-                } 
+                else v[s[i]-'a']--;
                 i++;
             }
             j++;
         }
         if(ans==123456789) return "";
-        return s.substr(st,e-st+1);
-
+        return s.substr(ai,aj-ai+1);
     }
 };
