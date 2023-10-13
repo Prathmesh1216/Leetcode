1f6b4dd2047bc00d3ee1b4371e02cbbc4b42c7ee
Prathmesh1216 <110608902+Prathmesh1216@users.noreply.github.com>
2023-10-13 16:26:54 +0530

    Time: 447 ms (75.48%) | Memory: 181.1 MB (28.75%) - LeetSync

--- 1300-critical-connections-in-a-network/critical-connections-in-a-network.cpp
+++ 1300-critical-connections-in-a-network/critical-connections-in-a-network.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    vector<int> vis,tin,low;
-    void dfs(int node,int parent,int& timer,vector<int> adj[]){
+    vector<vector<int>> ans,g;
+    vector<int> tin,vis,low;
+    void dfs(int node,int par,int& timer){
         vis[node] = 1;
         tin[node] = low[node] = timer++;
-        for(auto it : adj[node]){
-            if(it==parent) continue;
+        for(auto& it : g[node]){
+            if(it==par) continue;
             if(!vis[it]){
-                dfs(it,node,timer,adj);
-                low[node] = min(low[node],low[it]);
-                if(low[it]>tin[node]) ans.push_back({it,node});
+                dfs(it,node,timer);
+                low[node] = min(low[it],low[node]);
+                if(low[it]>tin[node]) ans.push_back({node,it});
             }
             else low[node] = min(low[node],tin[it]);
         }
+        return;
     }
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-        vector<int> adj[n];
-        for(auto it : connections){
-            int u = it[0];
-            int v = it[1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+        g.resize(n);
+        for(auto& it : connections){
+            g[it[0]].push_back(it[1]);
+            g[it[1]].push_back(it[0]);
         }
-        vis.resize(n,0);
-        tin.resize(n,-1);
-        low.resize(n,-1);
         int timer = 0;
+        vis.resize(n);tin.resize(n);low.resize(n,123456789);
         for(int i = 0;i<n;i++){
-            if(!vis[i]) dfs(i,-1,timer,adj);
+            if(!vis[i]) dfs(i,-1,timer);
         }
         return ans;
     }
