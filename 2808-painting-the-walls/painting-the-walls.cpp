55bf59014620848eab804caa18391fabb1dcb993
Prathmesh1216 <110608902+Prathmesh1216@users.noreply.github.com>
2023-10-14 12:12:35 +0530

    Time: 193 ms (27.46%) | Memory: 90.3 MB (73.95%) - LeetSync

--- 2808-painting-the-walls/painting-the-walls.cpp
+++ 2808-painting-the-walls/painting-the-walls.cpp
@@ -2,19 +2,17 @@ class Solution {
 public:
     int n;
     int dp[501][501];
-    int solve(int i,int walls,vector<pair<int,int>>& v){
+    int solve(int i,int walls,vector<int>& cost,vector<int>& time){
         if(walls>=n) return 0;
         if(i==n) return 1e9;
         if(dp[i][walls]!=-1) return dp[i][walls];
-        int ans = v[i].first + solve(i+1,walls + v[i].second + 1,v);
-        ans = min(ans,solve(i+1,walls,v));
+        int ans = cost[i] + solve(i+1,walls + time[i] + 1,cost,time);
+        ans = min(ans,solve(i+1,walls,cost,time));
         return dp[i][walls] = ans;
     }
     int paintWalls(vector<int>& cost, vector<int>& time) {
         n = cost.size();
-        vector<pair<int,int>> v;
         memset(dp,-1,sizeof(dp));
-        for(int i = 0;i<n;i++) v.push_back(make_pair(cost[i],time[i]));
-        return solve(0,0,v);
+        return solve(0,0,cost,time);
     }
 };
