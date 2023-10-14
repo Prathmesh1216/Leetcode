7006b239be9e2bf4d366e8355c7db2854957bfaf
Prathmesh1216 <110608902+Prathmesh1216@users.noreply.github.com>
2023-10-14 16:52:31 +0530

    Time: 60 ms (68.26%) | Memory: 15.1 MB (31.64%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -2,26 +2,26 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         set<vector<int>> st;
-        
-        unordered_map<long long,long long> mp;
-        for(auto& it : nums) mp[it]++;
+       // vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
         int n = nums.size();
         for(int i = 0;i<n;i++){
-            mp[nums[i]]--;
             for(int j = i+1;j<n;j++){
-                mp[nums[j]]--;
-                for(int k = j+1;k<n;k++){
-                    mp[nums[k]]--;
-                    if(mp[(long long)target-nums[i]-nums[j]-nums[k]]>0){
-                        vector<int> v = {nums[i],nums[j],nums[k],target-nums[i]-nums[j]-nums[k]};
-                        sort(v.begin(),v.end());
-                        st.insert(v);
+                int k = j+1;
+                int l = n-1;
+                long long tar = target - 1LL*nums[i] - 1LL*nums[j];
+                while(k<l){
+                    long long sum = nums[k] + nums[l];
+                    if(sum==tar){ 
+                        st.insert({nums[i],nums[j],nums[k],nums[l]});
+                        k++,l--;
+                        while(k<l && nums[k]==nums[k-1]) k++;
+                        while(k<l && nums[l]==nums[l+1]) l--;
                     }
-                    mp[nums[k]]++;
+                    else if(sum<tar) k++;
+                    else l--;
                 }
-                mp[nums[j]]++;
             }
-            mp[nums[i]]++;
         }
         vector<vector<int>> ans(st.begin(),st.end());
         return ans;
