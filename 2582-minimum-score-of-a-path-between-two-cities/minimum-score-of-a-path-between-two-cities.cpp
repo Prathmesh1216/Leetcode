028d96cdd3133367c1040b90b0e934a29a873fd2
Prathmesh1216 <110608902+Prathmesh1216@users.noreply.github.com>
2023-10-14 19:48:04 +0530

    Time: 332 ms (82.17%) | Memory: 104.7 MB (94.30%) - LeetSync

--- 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
@@ -1,25 +1,42 @@
 class Solution {
 public:
-    int minScore(int n, vector<vector<int>>& roads) {
-   vector<pair<int,int>> adj[n+1];
-   for(int i=0;i<roads.size();i++){
-       adj[roads[i][0]].push_back({roads[i][1],roads[i][2]});
-       adj[roads[i][1]].push_back({roads[i][0],roads[i][2]});
-   }
-        vector<int> dist(n+1,INT_MAX);
-        queue<int> q;
-        q.push(1);
-        
-        while(!q.empty()){
-            int node=q.front();
-            q.pop();
-            for(auto it:adj[node]){
-                if(dist[it.first]>min(dist[node],it.second)){
-                    dist[it.first]=min(dist[node],it.second);
-                    q.push(it.first);
-                }
-            }
+    vector<int> parent,rank,minscore;
+    int findPar(int node){
+        if(parent[node]==node) return node;
+        return parent[node] = findPar(parent[node]);
+    }
+    void unionbr(int u,int v){
+        u = findPar(u);
+        v = findPar(v);
+        if(u==v) return;
+        if(rank[u]<rank[v]){
+            parent[u] = v;
         }
-        return dist[n];
+        else if(rank[v]<rank[u]){
+            parent[v] = u;
+        }
+        else{
+            parent[v] = u;
+            rank[u]++;
+        }
+    }
+    int minScore(int n, vector<vector<int>>& roads) {
+        parent.resize(n+1);
+        rank.resize(n+1);
+        for(int i = 1;i<=n;i++) parent[i] = i;
+        minscore.resize(n+1,123456789);
+        for(auto& it : roads){
+            unionbr(it[0],it[1]);
+            int s1 = minscore[findPar(it[0])];
+            int s2 = minscore[findPar(it[1])];
+            unionbr(it[0],it[1]);
+            minscore[findPar(it[0])] = min({s1,s2,it[2]});
+            minscore[findPar(it[1])] = min({s1,s2,it[2]});
+        }
+        int ans = 123456789;
+        for(int i = 1;i<=n;i++){
+            if(findPar(i)==findPar(1)) ans = min(ans,minscore[i]);
+        }
+        return ans;
     }
 };
