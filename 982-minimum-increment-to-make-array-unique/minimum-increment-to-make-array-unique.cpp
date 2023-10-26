919f3124171b1996ed0c0b0fdc2cbf6023f8c81b
Prathmesh1216 <110608902+Prathmesh1216@users.noreply.github.com>
2023-10-27 02:38:11 +0530

    Time: 133 ms (54.73%) | Memory: 65.9 MB (66.18%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
@@ -2,27 +2,12 @@ class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        unordered_map<int,int> mp;
-        for(auto& it : nums) mp[it]++;
-        int mv = 1 + (*max_element(nums.begin(),nums.end()));
-        set<int> free;
-        int me = *min_element(nums.begin(),nums.end());
-      //  cout << me << " " << mv << endl;
-        for(int i = me+1;i<mv;i++) if(mp.find(i)==mp.end()) free.insert(i);
         int cnt = 0;
-        for(int i = 0;i<nums.size();i++){
-            if(mp[nums[i]]>1){
-                mp[nums[i]]--;
-                auto it = free.upper_bound(nums[i]);
-                int a = *it;
-                if(it==free.end()){
-                    cnt += (mv-nums[i]);
-                    mv++;
-                }
-                else{
-                    cnt += (a - nums[i]);
-                    free.erase(it);
-                }
+        for(int i = 1;i<nums.size();i++){
+            if(nums[i-1]>=nums[i]){
+                int t = nums[i-1] - nums[i] + 1;
+                cnt += t;
+                nums[i] += t;
             }
         }
         return cnt;
