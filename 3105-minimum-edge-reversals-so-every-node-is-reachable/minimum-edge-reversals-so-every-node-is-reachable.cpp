d9d43f031bb1dcb456dcbfb88dba4d13e2bfaca0
Prathmesh1216 <110608902+Prathmesh1216@users.noreply.github.com>
2023-10-30 12:09:37 +0530

    Time: 407 ms (85.97%) | Memory: 160.7 MB (98.93%) - LeetSync

--- 3105-minimum-edge-reversals-so-every-node-is-reachable/minimum-edge-reversals-so-every-node-is-reachable.cpp
+++ 3105-minimum-edge-reversals-so-every-node-is-reachable/minimum-edge-reversals-so-every-node-is-reachable.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int n;
-    int bfs(int src,vector<vector<pair<int,int>>>& g){
+    int bfs(vector<vector<pair<int,int>>>& g){
         queue<int> q;
-        int cnt = 0;
         vector<bool> vis(n);
-        q.push(src);
-        vis[src] = 1;
+        q.push(0); vis[0] = 1; int cnt = 0;
         while(!q.empty()){
-            int node = q.front(); q.pop();
+            int node = q.front(); 
+            q.pop();
             for(auto& it : g[node]){
                 if(!vis[it.first]){
                     vis[it.first] = 1;
@@ -23,25 +22,27 @@ public:
         n = N;
         vector<vector<pair<int,int>>> g(n);
         for(auto& it : edges){
-            g[it[1]].push_back({it[0],-1});
             g[it[0]].push_back({it[1],1});
+            g[it[1]].push_back({it[0],-1});
         }
         vector<int> count(n);
-        count[0] = bfs(0,g);
+        count[0] = bfs(g);
         queue<int> q;
         vector<bool> vis(n);
+        vis[0] = 1;
         q.push(0);
         while(!q.empty()){
             int node = q.front();
             q.pop();
-            for(auto& it : g[node]){
+            for(auto& it: g[node]){
                 if(!vis[it.first]){
-                    vis[it.first] = 1;
+                    vis[it.first]=1;
                     q.push(it.first);
-                    count[it.first] = count[node] + it.second;
+                    count[it.first] = it.second + count[node];
                 }
             }
         }
         return count;
+
     }
 };
