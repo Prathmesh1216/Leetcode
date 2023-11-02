9c8920f8ca174604ffc1eae9481e21b0cceb9e89
Prathmesh1216 <110608902+Prathmesh1216@users.noreply.github.com>
2023-11-02 15:56:20 +0530

    Time: 24 ms (91.19%) | Memory: 14.6 MB (95.69%) - LeetSync

--- 1466-jump-game-v/jump-game-v.cpp
+++ 1466-jump-game-v/jump-game-v.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-int f(int idx,vector<int> &arr,int d,vector<int> &dp){
-    if(idx>=arr.size()) return 0;
-    int ans=1;
-    if(dp[idx]!=-1) return dp[idx];
-    int a=max(0,idx-d);
-    int ans1=0;
-    for(int i=idx-1;i>=a;i--){
-        if(arr[i]<arr[idx]) ans=max(ans,1+f(i,arr,d,dp));
-        else break;
+    int n ;
+    int dp[1001];
+    int solve(int i,vector<int>& arr,int d){
+        int ans = 1;
+        if(dp[i]!=-1) return dp[i];
+        int ml = max(i-d,0), mr = min(i+d,n-1);
+        for(int j = i-1;j>=ml;j--){
+            if(arr[j]<arr[i]) ans = max(ans,1+solve(j,arr,d));
+            else break;
+        }
+        for(int j = i+1;j<=mr;j++){
+            if(arr[j]<arr[i]) ans = max(ans,1+solve(j,arr,d));
+            else break;
+        }
+        return dp[i] = ans;
+
     }
-    
-    for(int i=idx+1;i<arr.size() && i<=(idx+d);i++){
-        if(arr[i]<arr[idx]) ans1=max(ans1,1+f(i,arr,d,dp));
-        else break;
-    }
-    
-    return dp[idx]=max(ans,ans1);
-}
     int maxJumps(vector<int>& arr, int d) {
-        int ans=0;
-       vector<int> dp(arr.size(),-1);
-        for(int i=0;i<arr.size();i++){
-            ans=max(ans,f(i,arr,d,dp));
+        n = arr.size();
+        memset(dp,-1,sizeof(dp));
+        int ans= 0;
+        for(int i = 0;i<n;i++){
+            ans = max(ans,solve(i,arr,d));
         }
         return ans;
-        
     }
 };
