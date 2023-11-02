4e0036adbfb6411ca6bf5dbcb30766bde2597dc5
Prathmesh1216 <110608902+Prathmesh1216@users.noreply.github.com>
2023-11-02 14:33:23 +0530

    Time: 40 ms (29.33%) | Memory: 31.6 MB (81.32%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.cpp
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -1,32 +1,19 @@
 class Solution {
 public:
-    int n;
-    // int dp[50001];
-    // int solve(int i,vector<int>& arr){
-    //     if(i<0 || i>=n) return 0;
-    //     if(arr[i]==0) return 1;
-    //     if(dp[i]!=-1) return dp[i];
-    //     return solve(i+arr[i],arr) || solve(i-arr[i],arr);
-    // }
     bool canReach(vector<int>& arr, int start) {
-        n = arr.size();
+        int n = arr.size();
         queue<int> q;
-        vector<int> adj[n];
-        for(int i = 0;i<n;i++){
-            if(i+arr[i]<n){
-                adj[i].push_back(i+arr[i]);
-            }
-            if(i-arr[i]>=0) adj[i].push_back(i-arr[i]);
-        }
         q.push(start);
         vector<bool> vis(n);
+        vis[start] = 1;
         while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            if(arr[node]==0) return true;
-            vis[node] = 1;
-            for(auto it : adj[node]){
-                if(!vis[it]) q.push(it);
+            int i = q.front(); q.pop();
+            if(arr[i]==0) return true;
+            if(i-arr[i]>=0 && !vis[i-arr[i]]){
+                q.push(i-arr[i]); vis[i-arr[i]] = 1;
+            }
+            if(i+arr[i]<n && !vis[i+arr[i]]){
+                q.push(i+arr[i]); vis[i+arr[i]] = 1;
             }
         }
         return false;
