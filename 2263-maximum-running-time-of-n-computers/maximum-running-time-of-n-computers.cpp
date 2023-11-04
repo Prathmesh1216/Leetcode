d1055fc93b4b552300e18d1d8ca08f7d9feb161a
Prathmesh1216 <110608902+Prathmesh1216@users.noreply.github.com>
2023-11-04 23:47:47 +0530

    Time: 143 ms (30.64%) | Memory: 56.1 MB (47.27%) - LeetSync

---
+++ 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool  isValid(long long mid,int n,vector<int>& batteries){
+        int cnt = 0;
+        long long csum = 0;
+        for(auto& it : batteries){
+            csum += it;
+            if(csum>=mid){
+                csum -= mid;
+                cnt++;
+            }
+        }
+        return cnt>=n;
+    }
+    long long maxRunTime(int n, vector<int>& batteries) {
+        sort(batteries.begin(),batteries.end());
+        long long start = 0;
+        long long end =  accumulate(batteries.begin(),batteries.end(),0ll);
+        long long ans = -1;
+        while(start<=end){
+            long long mid = start + (end-start)/2;
+            if(isValid(mid,n,batteries)){
+                ans = mid;
+                start = mid + 1;
+            }
+            else end = mid -1 ;
+        }
+        return ans;
+    }
+};
