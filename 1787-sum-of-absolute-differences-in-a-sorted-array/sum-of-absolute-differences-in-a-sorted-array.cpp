8c056fe6b72eecec9f27bc080200c72443826fc1
Prathmesh1216 <110608902+Prathmesh1216@users.noreply.github.com>
2023-11-25 12:24:22 +0530

    Time: 70 ms (98.65%) | Memory: 83.6 MB (73.78%) - LeetSync

--- 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.cpp
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
         int n = nums.size();
-        vector<int> ps(n),ss(n);
-        for(int i = 1;i<n;i++) ps[i] = ps[i-1] + nums[i-1];
-        for(int i = n-2;i>=0;i--) ss[i] = ss[i+1] + nums[i+1];
+        int tsum = accumulate(nums.begin(),nums.end(),0);
+        int csum = 0;
         vector<int> ans(n);
         for(int i = 0;i<n;i++){
-            ans[i] = i*(nums[i]) - ps[i] + ss[i] - (n-i-1)*nums[i];
+            ans[i] = i*(nums[i]) - csum + (tsum-csum) - (n-i)*nums[i];
+            csum += nums[i];
         }
         return ans;
     }
