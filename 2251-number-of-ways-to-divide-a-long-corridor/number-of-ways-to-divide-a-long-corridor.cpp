808ea405178671068306e3cd87cdd40c596934c5
Prathmesh1216 <110608902+Prathmesh1216@users.noreply.github.com>
2023-11-28 13:38:21 +0530

    Time: 116 ms (83.43%) | Memory: 31.1 MB (73.71%) - LeetSync

--- 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.cpp
+++ 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.cpp
@@ -1,38 +1,26 @@
 class Solution {
 public:
-    int n;
-    vector<int> ps;
-    int mod = 1e9+7;
-    int numberOfWays(string& corridor) {
-        n = corridor.length();
-        ps.resize(n);
-        ps[0] = (corridor[0]== 'S');
-        for(int i = 1;i<n;i++){
-            if(corridor[i]=='S') ps[i] = 1;
-            ps[i] += ps[i-1];
-        }
-        if(ps[n-1]&1) return 0;
-        vector<int> ci;
-        int c = 1;
-        for(int i = 0;i<n;i++){
-            if(ps[i] == 2*c){
-                int kc = 1;
-                int j = i + 1;
-                while(j<n && corridor[j]!='S'){
-                    kc++;
-                    j++;
-                }
-                if(j!=n) ci.push_back(kc);
-                c++;
-            }
-        }
+    const int mod = 1e9+7;
+    int numberOfWays(string cor) {
         long long ans = 1;
-       // for(auto& it : ci) cout << it << endl;
-        if(ci.size()==0 && ps[n-1]>0) return 1;
-        if(ci.size()==0) return 0;
-        for(int i = 0;i<ci.size();i++){
-            ans *= (ci[i]*1LL);
-            ans %= mod;
+        int cnt = 0;
+        for(auto& it : cor){
+            if(it=='S') cnt++;
+        }
+        if(cnt&1 || (cnt==0)) return 0;
+        int cc = 0;
+        int i = 0;
+        while(i<cor.length()){
+            if(cor[i]=='S') cc++;
+            if(cc==2){
+                int j = i + 1;
+                while(j<cor.length() && cor[j]!='S') j++;
+                if(j==cor.length()) return ans;
+                ans = (ans*1ll*(j-i))%mod;
+                i = j;
+                cc= 1;
+            }
+            i++;
         }
         return ans%mod;
     }
